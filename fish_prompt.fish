8993b65e19f688e76b743ca2873e5e75a7798daf
rektrex <psaimanideep@gmail.com>
2019-10-13 16:23:37 +0530

    make git info show only when in a git repository

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -2,26 +2,31 @@ function fish_prompt
     set -l git_color normal
     set -l dollar_color normal
     set -l untracked ""
+    set -l branch_name ""
 
-    if git_is_staged
-        set git_color green
+    if git_is_repo
+        set branch_name (git_branch_name)
 
-        if git_is_dirty
-            set dollar_color red
+        if git_is_staged
+            set git_color green
+
+            if git_is_dirty
+                set dollar_color red
+            end
+
+        else if git_is_dirty or git_is_touched
+            set git_color red
         end
 
-    else if git_is_dirty or git_is_touched
-        set git_color red
-    end
+        set untracked_files (git_untracked_files)
 
-    set untracked_files (git_untracked_files)
-
-    if test "$untracked_files" -gt 0
-        set untracked "|$untracked_files"
+        if test "$untracked_files" -gt 0
+            set untracked "|$untracked_files"
+        end
     end
 
     set_color $git_color
-    printf '(%s%s)' (git_branch_name) $untracked
+    printf '%s%s' $branch_name $untracked
     set_color $dollar_color
-	echo -n '$ '
+	echo -n ' $ '
 end
