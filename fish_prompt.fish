d81623a3cc34d14ac2c23d9583b086dbd6a47e39
rektrex <psaimanideep@gmail.com>
2019-10-13 16:16:14 +0530

    version 1.0

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,3 +1,27 @@
 function fish_prompt
+    set -l git_color normal
+    set -l dollar_color normal
+    set -l untracked ""
+
+    if git_is_staged
+        set git_color green
+
+        if git_is_dirty
+            set dollar_color red
+        end
+
+    else if git_is_dirty or git_is_touched
+        set git_color red
+    end
+
+    set untracked_files (git_untracked_files)
+
+    if test "$untracked_files" -gt 0
+        set untracked "|$untracked_files"
+    end
+
+    set_color $git_color
+    printf '(%s%s)' (git_branch_name) $untracked
+    set_color $dollar_color
 	echo -n '$ '
 end
